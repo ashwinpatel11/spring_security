6cc526e87565c6afdb3339ee96db0cfd5cde2a30
ashwinpatel11 <ashvin007patel@gmail.com>
2022-12-30 18:01:00 +0530

    package name change

--- src/test/java/com/example/security/ContactMgtApplicationTests.java
+++ src/test/java/com/example/security/ContactMgtApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.webosmotic;
+package com.example.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebosmoticApplicationTests {
+class ContactMgtApplicationTests {
 
 	@Test
 	void contextLoads() {
